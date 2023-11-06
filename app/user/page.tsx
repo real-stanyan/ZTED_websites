f1b77991ba8a13069432316763ac08dbf3a61be4
Stan Yan <stanhavenoidea@gmail.com>
2023-11-06 23:35:04 +1100

    user css advance fix

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { Noto_Serif_SC } from "next/font/google";
+
+const Noto = Noto_Serif_SC({ weight: ["400", "600"], subsets: ["latin"] });
 
 export default function UserRegistration() {
   const router = useRouter();
   const [page, setPage] = useState("login");
   const [registerError, setRegisterError] = useState("");
   const [loginError, setLoginError] = useState("");
-  const [useLogin, setUserLogin] = useState<UserLogin>({
+  const [userLogin, setUserLogin] = useState<UserLogin>({
     email: "",
     password: "",
   });
@@ -19,8 +22,89 @@ export default function UserRegistration() {
     password: "",
     confirmPassword: "",
   });
+  const [loginInputError, setLoginInputError] = useState({
+    email: false,
+    password: false,
+  });
+  const [registerInputError, setRegisterInputError] = useState({
+    name: false,
+    email: false,
+    phoneNum: false,
+    password: false,
+    confirmPassword: false,
+  });
+
+  useEffect(() => {
+    if (loginError.includes("邮箱")) {
+      setLoginInputError({ password: false, email: true });
+    }
+    if (loginError.includes("密码")) {
+      setLoginInputError({ password: true, email: false });
+    }
+  }, [loginError]);
+
+  useEffect(() => {
+    if (registerError.includes("姓名")) {
+      setRegisterInputError({
+        email: false,
+        phoneNum: false,
+        password: false,
+        confirmPassword: false,
+        name: true,
+      });
+    }
+    if (registerError.includes("邮箱")) {
+      setRegisterInputError({
+        phoneNum: false,
+        password: false,
+        confirmPassword: false,
+        name: false,
+        email: true,
+      });
+    }
+    if (registerError.includes("手机号")) {
+      setRegisterInputError({
+        phoneNum: true,
+        email: false,
+        password: false,
+        confirmPassword: false,
+        name: false,
+      });
+    }
+    if (registerError.includes("密码")) {
+      setRegisterInputError({
+        name: false,
+        phoneNum: true,
+        email: false,
+        password: true,
+        confirmPassword: true,
+      });
+    }
+  }, [registerError]);
+
   // 注册Function
   const handleRegister = async () => {
+    if (userRegister.name === "") {
+      setRegisterError("请输入姓名");
+      return;
+    }
+    if (userRegister.email === "") {
+      setRegisterError("请输入邮箱");
+      return;
+    }
+    if (userRegister.phoneNum === "") {
+      setRegisterError("请输入手机号");
+      return;
+    }
+    if (userRegister.password === "") {
+      setRegisterError("请输入密码");
+      return;
+    }
+    if (userRegister.confirmPassword === "") {
+      setRegisterError("请确认密码");
+      return;
+    }
+
     const res = await fetch("http://localhost:8080/ZTED/user/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -45,10 +129,19 @@ export default function UserRegistration() {
   };
   // 登陆function
   const handleLogin = async () => {
+    if (userLogin.email === "") {
+      setLoginError("请输入邮箱");
+      return;
+    }
+    if (userLogin.password === "") {
+      setLoginError("请输入密码");
+      return;
+    }
+
     const res = await fetch("http://localhost:8080/ZTED/user/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(useLogin),
+      body: JSON.stringify(userLogin),
       // credentials: "include",
     });
 
@@ -77,7 +170,9 @@ export default function UserRegistration() {
           {/* 注册姓名 */}
           <div className="w-[50%]">
             <input
-              className="w-full h-[50px] p-[5px] border border-black rounded bg-transparent text-black my-[1vw]"
+              className={`w-full h-[50px] p-[5px] border border-black rounded bg-transparent text-black my-[1vw] ${
+                registerInputError.name ? "border-red-500" : ""
+              }`}
               type="text"
               placeholder="姓名"
               onChange={(e) =>
@@ -88,7 +183,9 @@ export default function UserRegistration() {
           {/* 注册邮箱 */}
           <div className="w-[50%]">
             <input
-              className="w-full h-[50px] p-[5px] border border-black rounded bg-transparent text-black my-[1vw]"
+              className={`w-full h-[50px] p-[5px] border border-black rounded bg-transparent text-black my-[1vw] ${
+                registerInputError.email ? "border-red-500" : ""
+              }`}
               type="email"
               placeholder="邮箱"
               onChange={(e) =>
@@ -99,7 +196,9 @@ export default function UserRegistration() {
           {/* 注册手机号 */}
           <div className="w-[50%]">
             <input
-              className="w-full h-[50px] p-[5px] border border-black rounded bg-transparent text-black my-[1vw]"
+              className={`w-full h-[50px] p-[5px] border border-black rounded bg-transparent text-black my-[1vw] ${
+                registerInputError.phoneNum ? "border-red-500" : ""
+              }`}
               type="text"
               placeholder="手机号"
               onChange={(e) =>
@@ -110,7 +209,9 @@ export default function UserRegistration() {
           {/* 注册密码 */}
           <div className="w-[50%]">
             <input
-              className="w-full h-[50px] p-[5px] border border-black rounded bg-transparent text-black my-[1vw]"
+              className={`w-full h-[50px] p-[5px] border border-black rounded bg-transparent text-black my-[1vw] ${
+                registerInputError.password ? "border-red-500" : ""
+              }`}
               type="password"
               placeholder="密码"
               onChange={(e) =>
@@ -121,7 +222,9 @@ export default function UserRegistration() {
 
           <div className="w-[50%]">
             <input
-              className="w-full h-[50px] p-[5px] border border-black rounded bg-transparent text-black my-[1vw]"
+              className={`w-full h-[50px] p-[5px] border border-black rounded bg-transparent text-black my-[1vw] ${
+                registerInputError.confirmPassword ? "border-red-500" : ""
+              }`}
               type="password"
               placeholder="确认密码"
               onChange={(e) =>
@@ -132,9 +235,13 @@ export default function UserRegistration() {
               }
             />
           </div>
-          {registerError !== "" && (
+          {/* {registerError !== "" && (
             <div className="w-[50%] text-red-500">{registerError}</div>
-          )}
+          )} */}
+          {/* 注册报错 */}
+          <div className="w-full min-h-[30px] text-center text-red-500">
+            {registerError}
+          </div>
           <div
             className="w-[20%] h-[50px] leading-[50px] border border-black text-black text-center rounded hover:text-white hover:bg-black"
             onClick={handleRegister}
@@ -148,28 +255,36 @@ export default function UserRegistration() {
           <h1 className="text-black text-[4vw] my-[2vw]">用户登陆</h1>
           <div className="w-[50%]">
             <input
-              className="w-full h-[50px] p-[5px] border border-black rounded bg-transparent text-black my-[1vw]"
+              className={`w-full h-[50px] p-[5px] border border-black rounded bg-transparent text-black my-[1vw] ${
+                loginInputError.email ? "border-red-500" : ""
+              }`}
               type="text"
               placeholder="邮箱"
               onChange={(e) => {
-                setUserLogin({ ...useLogin, email: e.target.value });
+                setUserLogin({ ...userLogin, email: e.target.value });
               }}
             />
           </div>
           <div className="w-[50%]">
             <input
-              className="w-full h-[50px] p-[5px] border border-black rounded bg-transparent text-black my-[1vw]"
+              className={`w-full h-[50px] p-[5px] border border-black rounded bg-transparent text-black my-[1vw] ${
+                loginInputError.password ? "border-red-500" : ""
+              }`}
               type="text"
               placeholder="密码"
               onChange={(e) => {
-                setUserLogin({ ...useLogin, password: e.target.value });
+                setUserLogin({ ...userLogin, password: e.target.value });
               }}
             />
           </div>
 
-          {loginError !== "" && (
+          {/* {loginError !== "" && (
             <div className="w-[50%] text-red-500">{loginError}</div>
-          )}
+          )} */}
+          {/* 登陆报错 */}
+          <div className="w-full min-h-[30px] text-center text-red-500">
+            {loginError}
+          </div>
           <div
             className="w-[20%] h-[50px] leading-[50px] border border-black text-black text-center rounded hover:text-white hover:bg-black"
             onClick={handleLogin}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,13 +20,23 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       keyframes: {
-        swiper: {
-          "0%": { left: "0vw" },
-          "100%": { left: "200vw" },
+        // swiper: {
+        //   "0%": { left: "0vw" },
+        //   "100%": { left: "200vw" },
+        // },
+        wrongInput: {
+          "25%": { left: "-10px" },
+          "40%": { borderColor: "red" },
+          "50%": { left: "10px" },
+          "60%": { borderColor: "red" },
+          "75%": { left: "-10px" },
+          "90%": { borderColor: "red" },
+          "100%": { left: "0px" },
         },
       },
       animation: {
         swiper: "swiper 20s ease-in-out infinite alternate", // 2s duration and ease-in-out timing function as an example
+        wrongInput: "wrongInput 0.5s ease-in-out 1",
       },
     },
   },
